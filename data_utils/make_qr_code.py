90593f964df9067984d51b03e161a87958d9259e
RucLuke <lixinkuo@ruc.edu.cn>
2018-10-26 10:39:44 +0800

    credit_card

--- data_utils/make_qr_code.py
+++ data_utils/make_qr_code.py
@@ -2,6 +2,8 @@
 import qrcode
 import pandas as pd
 import uuid
+from pathlib import Path
+import os
 
 
 def make_qr_code(item_id, guid):
@@ -18,27 +20,33 @@ def make_qr_code(item_id, guid):
     img = qr.make_image()
     img = img.convert("RGBA")
     print(url)
+    directory = Path("../pic")
+    if not directory.exists():
+        os.makedirs(directory)
+
     img.save("../pic/%d_%s.png" % (item_id, guid))
 
 
 def save_uuid():
     # 保存guid 为excel
-    guid_array = []
-    for i in range(1, 31):
-        dic = {'id': i, "guid": str(uuid.uuid4()).replace("-", "")}
-        guid_array.append(dic)
     file_path = r'guid.xlsx'
-    writer = pd.ExcelWriter(file_path)
-    df = pd.DataFrame(guid_array)
-    df.to_excel(writer, columns=['id', 'guid'], index=False, encoding="utf-8", sheet_name="Sheet")
-    writer.save()
+    directory = Path(file_path)
+    if not directory.exists():
+        guid_array = []
+        for i in range(1, 31):
+            dic = {'id': i, "guid": str(uuid.uuid4()).replace("-", "")}
+            guid_array.append(dic)
+        writer = pd.ExcelWriter(file_path)
+        df = pd.DataFrame(guid_array)
+        df.to_excel(writer, columns=['id', 'guid'], index=False, encoding="utf-8", sheet_name="Sheet")
+        writer.save()
 
 
 if __name__ == '__main__':
     """
     方法主入口
     """
-    # save_uuid()
+    save_uuid()
     guid_list = pd.read_excel("guid.xlsx")
     for guid_item in guid_list.values:
         make_qr_code(guid_item[0], guid_item[1])
--- logistics_regression/credit_card.py
+++ logistics_regression/credit_card.py
@@ -1,10 +1,46 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+
+def preview_data(data):
+    # 初步观察样本的分布规则
+    count_classes = pd.value_counts(data['Class'], sort=True).sort_index()
+    print(count_classes)
+    count_classes.plot(kind='bar')
+    plt.title('Fraud class histogram')
+    plt.xlabel("Class")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def process_data(data):
+    # 预处理，标准化数据列
+    # reshape 中 -1，的意思是自动补全
+    data["normAmount"] = StandardScaler().fit_transform(data["Amount"].values.reshape(-1, 1))
+    data = data.drop(['Time', 'Amount'], axis=1)
+    x = data.loc[:, data.columns != "Class"]
+    y = data.loc[:, data.columns == "Class"]
+
+    number_records_fraud = len(data[data.Class == 1])
+    fraud_indices = np.array(data[data.Class == 1].index)
+
+    normal_indices = data[data.Class == 0].index
+
+    random_normal_indices = np.random.choice(normal_indices, number_records_fraud, replace=False)
+    random_normal_indices = np.array(random_normal_indices)
+    # 合并
+    under_sample_indices = np.concatenate([fraud_indices, random_normal_indices])
+    under_sample_data = data.iloc[under_sample_indices, :]
+    x_undersample = under_sample_data.loc[:, under_sample_data.columns != 'Class']
+    y_undersample = under_sample_data.loc[:, under_sample_data.columns == 'Class']
+    return data
+
 
 if __name__ == '__main__':
     credit_card_data = pd.read_csv("data/creditcard.csv")
-    print(len(credit_card_data))
-    print(credit_card_data.head(2))
-    print(credit_card_data.loc[0, :])
+    # preview_data(credit_card_data)
+    standardized_data = process_data(credit_card_data)
 
+    # print(standardized_data.head())
