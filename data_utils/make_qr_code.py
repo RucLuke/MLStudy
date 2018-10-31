36755dc6aa050c4a495fc0d888efa1c4d61de648
RucLuke <lixinkuo@ruc.edu.cn>
2018-10-31 18:44:42 +0800

    change qr_code

--- data_utils/make_qr_code.py
+++ data_utils/make_qr_code.py
@@ -4,6 +4,29 @@ import pandas as pd
 import uuid
 from pathlib import Path
 import os
+from PIL import Image
+
+
+def add_icon(img, icon_path):
+    """
+    为二维码添加icon
+    """
+    icon = Image.open(icon_path)
+    img_w, img_h = img.size
+    factor = 6
+    size_w = int(img_w / factor)
+    size_h = int(img_h / factor)
+
+    icon_w, icon_h = icon.size
+    if icon_w > size_w:
+        icon_w = size_w
+    if icon_h > size_h:
+        icon_h = size_h
+    icon = icon.resize((icon_w, icon_h), Image.ANTIALIAS)
+
+    w = int((img_w - icon_w) / 2)
+    h = int((img_h - icon_h) / 2)
+    img.paste(icon, (w, h), icon)
 
 
 def make_qr_code(item_id, guid):
@@ -23,7 +46,7 @@ def make_qr_code(item_id, guid):
     directory = Path("../pic")
     if not directory.exists():
         os.makedirs(directory)
-
+    add_icon(img, "../pic/nuo.png")
     img.save("../pic/%d_%s.png" % (item_id, guid))
 
 
