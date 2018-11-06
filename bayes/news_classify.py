aed2b7b4e7cba0955ba8b288986956e1556c0478
RucLuke <lixinkuo@ruc.edu.cn>
2018-11-06 18:14:57 +0800

    update news_classify

--- bayes/news_classify.py
+++ bayes/news_classify.py
@@ -1,10 +1,16 @@
-import pandas as pd
 import jieba
-import numpy as np
-import matplotlib.pyplot as plt
+import gensim
 import matplotlib
-from wordcloud import WordCloud
+import numpy as np
+import pandas as pd
 import jieba.analyse
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from wordcloud import WordCloud
+from gensim import corpora
 
 
 def cut_words():
@@ -70,11 +76,78 @@ def analyse_words():
     print(" ".join(jieba.analyse.extract_tags(content_s_str, topK=5, withWeight=False)))
 
 
+def lda_model():
+    """
+    LDA 主题模型
+    :return:
+    """
+    dictionary = corpora.Dictionary(contents_clean_list)
+    corpus = [dictionary.doc2bow(sentence) for sentence in contents_clean_list]
+    lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=20)
+    for topic in (lda.print_topics(num_topics=20, num_words=5)):
+        print(topic[1])
+
+
+def content_label():
+    """
+    利用贝叶斯进行文本分类
+    :return:
+    """
+    df_train = pd.DataFrame({"contents_clean": contents_clean_list, "label": df_news['category']})
+    # print(df_train.label.unique())
+    label_mapping = {"汽车": 1, "财经": 2, "科技": 3, "健康": 4, "体育": 5, "教育": 6, "文化": 7, "军事": 8, "娱乐": 9, "时尚": 0}
+    df_train['label'] = df_train['label'].map(label_mapping)
+    x_train, x_test, y_train, y_test = train_test_split(df_train["contents_clean"].values,
+                                                        df_train["label"].values.ravel())
+    words = []
+    for line_index in range(len(x_train)):
+        try:
+            words.append(' '.join(x_train[line_index]))
+        except:
+            print(line_index, words)
+    print(words[0])
+    vec = CountVectorizer(analyzer='word', max_features=4000, lowercase=False)
+    vec.fit(words)
+    classifier = MultinomialNB()
+    classifier.fit(vec.transform(words), y_train)
+    test_words = []
+    for line_index in range(len(x_test)):
+        try:
+            test_words.append(' '.join(x_test[line_index]))
+        except:
+            print(line_index)
+    score = classifier.score(vec.transform(test_words), y_test)
+    print(score)
+
+    vectorizer = TfidfVectorizer(analyzer='word', max_features=4000, lowercase=False)
+    vectorizer.fit(words)
+    classifier = MultinomialNB()
+    classifier.fit(vectorizer.transform(words), y_train)
+    score = classifier.score(vectorizer.transform(test_words), y_test)
+    print(score)
+
+
+def count_vector():
+    """
+    CV
+    :return:
+    """
+    texts = ["dog cat fish", "dog cat cat", "fish bird", "bird"]
+    cv = CountVectorizer()
+    cv_fit = cv.fit_transform(texts)
+    print(cv.get_feature_names())
+    print(cv_fit.toarray())
+    print(cv_fit.toarray().sum(axis=0))
+
+
 if __name__ == '__main__':
     df_news = pd.read_table("val.txt", names=["category", "theme", "URL", "content"], encoding="utf-8")
     df_news = df_news.dropna()
     contents = df_news.content.values.tolist()
     contents_clean_list, all_word_list = clean_data(cut_words())
     df_content_list = pd.DataFrame({"contents_clean": contents_clean_list})
-    word_count()
-    analyse_words()
+    # word_count()
+    # analyse_words()
+    # lda_model()
+    content_label()
+    # count_vector()
