09f2fb50a73f6beec1473ea24988c7b5b1407501
RucLuke <lixinkuo@ruc.edu.cn>
2018-11-07 15:04:27 +0800

    update news_classify

--- bayes/news_classify.py
+++ bayes/news_classify.py
@@ -134,7 +134,7 @@ def count_vector():
     :return:
     """
     texts = ["dog cat fish", "dog cat cat", "fish bird", "bird"]
-    cv = CountVectorizer()
+    cv = CountVectorizer(ngram_range=(1, 4))
     cv_fit = cv.fit_transform(texts)
     print(cv.get_feature_names())
     print(cv_fit.toarray())
